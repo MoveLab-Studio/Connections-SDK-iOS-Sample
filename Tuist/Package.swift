766cbfca2c7c5aa6ab425ff6eae8adb79007987d
Niek Haarman <niek@movelab.studio>
2026-09-25 09:49:20 +0200

    Bump Connections SDK to 3.7.14

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/MoveLab-Studio/Connections-SDK-Apple-Distribution.git",
-            revision: "3.7.13"
+            revision: "3.7.14"
         ),
         .package(
             url: "https://github.com/MoveLab-Studio/Domain-iOS.git",
